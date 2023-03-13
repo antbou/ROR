1d314d94265bdf03b60623b855eec8ba714009bc
Anthony <bouillant.anthony@gmail.com>
2023-03-13 11:32:42 +0100

    Create people

---
+++ app/models/person.rb
@@ -0,0 +1,9 @@
+class Person < ApplicationRecord
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true
+    validates :adress, presence: true
+    validates :city, presence: true
+    belongs_to :city
+    
+end
