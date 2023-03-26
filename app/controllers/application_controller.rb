bef1cc4b988850f5559f989e85c8d493e2e362e7
antbou <bouillant.anthony@gmail.com>
2023-03-27 01:56:17 +0200

    Add flash message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,19 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
   devise_group :user, contains: %i[student teacher]
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [
-                                        :first_name,
-                                        :last_name,
-                                        :adress,
-                                        { city_attributes: %i[name zip_code] }
-                                      ])
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [
+        :first_name,
+        :last_name,
+        :adress,
+        { city_attributes: %i[name zip_code] }
+      ]
+    )
   end
 end
